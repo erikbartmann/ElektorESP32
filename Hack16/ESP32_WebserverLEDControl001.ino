4d6d278567b2b147f75dddd15a0e96f69260eb72
Erik Bartmann <erik.bartmann@yahoo.de>
2018-04-14 16:17:18 +0200

    Create ESP32_WebserverLEDControl001.ino

---
+++ Hack16/ESP32_WebserverLEDControl001.ino
@@ -0,0 +1,93 @@
+#include <WiFi.h>
+#define LED1 21 // LED-Pin 1
+#define LED2 22 // LED-Pin 2
+#define LED3 19 // LED-Pin 3
+
+const char* ssid     = "<SSID>";
+const char* password = "<Password>";
+String request       = ""; // HTTP-Request
+
+WiFiServer server(80); // Webserver-Objekt (Port 80)
+
+void setup() {
+  Serial.begin(9600); // Init Serial-Monitor
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  Serial.print("Verbindungsaufnahme zu SSID:  ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("."); delay(500);
+  }
+  Serial.println();
+  Serial.println("WiFi-Verbindung ok.");
+  Serial.print("IP-Adresse: ");
+  Serial.println(WiFi.localIP());
+  server.begin(); // Starte Server
+}
+
+void loop() {
+  WiFiClient client = server.available(); // Lauscht auf neue Clientanfragen
+  if (client) {                           // falls ein Client anfragt
+    Serial.println("Neuer Client.");      // erfolgt eine Nachricht im Serial-Monitor
+    String currentLine = "";              // Variable für die Client-Daten
+    while (client.connected()) {          // Schleife, solange Client verbunden
+      if (client.available()) {           // Sind Daten zum Lesen vorhanden
+        char c = client.read();           // dann wird ein Byte gelesen
+        Serial.write(c);                  // Ausgabe im Serial-Monitor
+        request += c;                     // HTTP-Request
+        if (c == '\n') {                  // Wenn Byte ist ein Zeilenumbruch...
+          if (currentLine.length() == 0) {
+            // HTTP-Header starten immer mit dem Response Code (HTTP/1.1 200 OK)
+            // und dem Content-Type gefolgt von einer Leerzeile
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+            // GPIO-Ansteuerung
+            ledControl(request);            
+            // Bootstrap-Resource einbinden
+            client.println("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>");
+            client.println("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>");
+            client.println("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>");
+            // Hier kommen die LED-Botton
+            client.println("<h1>GPIO-Control</h1>");
+            client.println("GPIO 21: ");
+            client.println("<div class='btn-group'>");
+            client.println("<a href='/21/ON' class='btn btn-success'>On</a>");
+            client.println("<a href='/21/OFF' class='btn btn-danger'>Off</a>");
+            client.println("</div><br />");
+            client.println("GPIO 22: ");
+            client.println("<div class='btn-group'>");
+            client.println("<a href='/22/ON' class='btn btn-success'>On</a>");
+            client.println("<a href='/22/OFF' class='btn btn-danger'>Off</a>");
+            client.println("</div><br />");
+            client.println("GPIO 19: ");
+            client.println("<div class='btn-group'>");
+            client.println("<a href='/19/ON' class='btn btn-success'>On</a>");
+            client.println("<a href='/19/OFF' class='btn btn-danger'>Off</a>");
+            client.println("</div><br />");
+            // Der HTTP-Response endet mit einer Leerzeile
+            client.println();
+            break; // Die Loop wird verlassen
+          } else { // falls eine neue Zeile kommt, wird die alte gelöscht
+            currentLine = "";
+          }
+        } else if (c != '\r') { // Alles, was nicht Carriage Return ist,
+          currentLine += c;     // wird am Ende der currentLine hinzugefügt
+        }
+      }
+    }
+    request = "";  // HTTP-Request leeren
+    client.stop(); // Schließe Verbindung
+    Serial.println("Client-Verbindung geschlossen.");
+  }
+}
+
+void ledControl(String setLED) {
+  int pos1 = setLED.indexOf('/') + 1;
+  int pos2 = setLED.indexOf('/', pos1 + 1);
+  int pin = setLED.substring(pos1, pos2).toInt();
+  String status = setLED.substring(pos2 + 1, setLED.indexOf(' ', 5));
+  digitalWrite(pin, status=="ON"?HIGH:LOW);
+}
