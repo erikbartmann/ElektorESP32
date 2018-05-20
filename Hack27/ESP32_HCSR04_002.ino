b5426b4f9c22c01f43a9c09d8e562782a19f3601
Erik Bartmann <erik.bartmann@yahoo.de>
2018-05-20 10:43:35 +0200

    Create ESP32_HCSR04_002.ino

---
+++ Hack27/ESP32_HCSR04_002.ino
@@ -0,0 +1,22 @@
+#define trigPin 21 // Trigger-Pin
+#define echoPin 22 // Echo-Pin
+long zeit, cm;
+ 
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+ 
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(3);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  pinMode(echoPin, INPUT);
+  zeit = pulseIn(echoPin, HIGH);
+  cm = (zeit/2) / 29.4;
+  Serial.println(cm);
+  delay(50);
+}
