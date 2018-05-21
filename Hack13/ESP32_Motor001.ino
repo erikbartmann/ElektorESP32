ecc613f1cf4cf318754b588f410f75b7bfb16c50
Erik Bartmann <erik.bartmann@yahoo.de>
2018-05-21 13:13:33 +0200

    Update ESP32_Motor001.ino

--- Hack13/ESP32_Motor001.ino
+++ Hack13/ESP32_Motor001.ino
@@ -1,7 +1,7 @@
-#define MotorPin 25 // Motor-Pin
-#define TouchOn  T2 // Touch T2 -> IO2
-#define TouchOff T3 // Touch T3 -> IO15
-#define Theshold 15 // Ansprechschwelle
+#define MotorPin 25  // Motor-Pin
+#define TouchOn  T2  // Touch T2 -> IO2
+#define TouchOff T3  // Touch T3 -> IO15
+#define Threshold 15 // Ansprechschwelle
 
 bool motorDrive = false; // Flag für Motor
 
@@ -12,8 +12,8 @@ void setup() {
 void loop() {
   byte touchValueOn  = touchRead(TouchOn);
   byte touchValueOff = touchRead(TouchOff);
-  if(touchValueOn  < Theshold) motorDrive = true;
-  if(touchValueOff < Theshold) motorDrive = false;
+  if(touchValueOn  < Threshold) motorDrive = true;
+  if(touchValueOff < Threshold) motorDrive = false;
   if(motorDrive) 
     digitalWrite(MotorPin, HIGH);
   else
